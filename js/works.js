80d7b4d3c074f3f06b2036a70180040d00820d89
Thomas Tao <jiachen215@qq.com>
2018-08-21 16:41:01 +0800

    update

--- js/works.js
+++ js/works.js
@@ -8,12 +8,12 @@ var plusDivs = function(n) {
 var showDivs = function(n) {
 	var i;
 	var x = document.getElementsByClassName("mySlides");
-	if (n > x.length) {slideIndex = 1}    
+	if (n > x.length) {slideIndex = 1}
 	if (n < 1) {slideIndex = x.length}
 	for (i = 0; i < x.length; i++) {
-	x[i].style.display = "none";  
+	x[i].style.display = "none";
 	}
-	x[slideIndex-1].style.display = "block";  
+	x[slideIndex-1].style.display = "block";
 }
 showDivs(slideIndex);
 
@@ -25,29 +25,29 @@ $(".popImg").on("touchstart", function(e) {
         if (!e.defaultPrevented) {
             e.preventDefault();
         }
-    }   
+    }
     startX = e.originalEvent.changedTouches[0].pageX,
     startY = e.originalEvent.changedTouches[0].pageY;
 });
-$(".popImg").on("touchend", function(e) {         
+$(".popImg").on("touchend", function(e) {
     // 判断默认行为是否可以被禁用
     if (e.cancelable) {
         // 判断默认行为是否已经被禁用
         if (!e.defaultPrevented) {
             e.preventDefault();
         }
-    }               
+    }
     moveEndX = e.originalEvent.changedTouches[0].pageX,
     moveEndY = e.originalEvent.changedTouches[0].pageY,
     X = moveEndX - startX,
     Y = moveEndY - startY;
     //左滑
-    if ( X > 0 ) {    
-        plusDivs(-1);             
+    if ( X > 0 ) {
+        plusDivs(-1);
     }
     //右滑
-    else if ( X < 0 ) { 
-        plusDivs(1);  
+    else if ( X < 0 ) {
+        plusDivs(1);
     }
 });
 
@@ -60,16 +60,16 @@ $(document).ready(function(){
 	})
 	$('.workArea').click(function(e) {
         e.stopPropagation();
-        
+
         var key = this.getAttribute('id')
 
-        
+
 
         //加入Item
         
 
         var popUpImgs = JSON.parse(localStorage.getItem(key))
-        
+
         var item = document.createElement('div')
         item.setAttribute('id','popUpImgs')
         item.setAttribute('class','popImg')
@@ -80,7 +80,7 @@ $(document).ready(function(){
             img.setAttribute('class','mySlides')
             if (i==0) {img.setAttribute('style','display:block')}
             else {
-                img.setAttribute('style','display:none') 
+                img.setAttribute('style','display:none')
             }
             item.appendChild(img);
 
@@ -90,10 +90,10 @@ $(document).ready(function(){
         var parentDiv = popUpImgDiv.parentNode;
         parentDiv.replaceChild(item,popUpImgDiv)
 
-        
+
 
         $('.mask').show();
-        
+
 
 		$('.popUp').show();
 	});
@@ -101,4 +101,3 @@ $(document).ready(function(){
 });
 
 //移动端：当workArea距离顶部小于（px）时候变彩色
-
